5a3200511b93e7dd424de936f49bae6ac47704c0
Miguel Cortes <macortesn@unal.edu.co>
2017-09-27 08:02:03 -0500

    Ejemplo algunas funciones del metodo translate de I18n

--- config/application.rb
+++ config/application.rb
@@ -10,16 +10,16 @@ module Example
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    
+
     # Where the I18n library should search for translation files
     I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
- 
+
     # Whitelist locales available for the application
-    I18n.available_locales = [:en, :pirate]
- 
+    I18n.available_locales = [:en, :pirate , :nl]
+
     # Set default locale to something other than :en
     I18n.default_locale = :en
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
